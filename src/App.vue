e2e781ce87266b231598fc42b7716d7c08bed8e6
antokriswandi <anto641@guru.smk.belajar.id>
2025-10-09 10:22:05 +0700

    Test Commit

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
 <template>
   <h1 class="text-3xl font-bold underline">Hello dunia</h1>
+  <div class="p-8 rounded-lg bg-slate-900 text-slate-50 shadow-m">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus officiis qui fugit vel soluta id dolorum dicta molestiae maiores voluptas.
+  </div>
 </template>
